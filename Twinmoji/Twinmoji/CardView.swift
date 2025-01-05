1ba1c8c65295d420d98814829a7998acfb01b95b
Oriol Angelet <uriaeri@gmail.com>
2025-01-05 16:54:43 +0100

    CardView first iteration

--- Twinmoji/Twinmoji/CardView.swift
+++ Twinmoji/Twinmoji/CardView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    // the emojis on card
+    var card: [String]
+    
+    var rows: Int {
+        if card.count == 12 {
+            4
+        } else {
+            3
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // i: rows, j: columns (always 3)
+        Grid(horizontalSpacing: 0, verticalSpacing: 0) {
+            ForEach(0..<rows, id: \.self) { i in
+                GridRow {
+                    ForEach(0..<3) { j in
+                        let text = card[i * 3 + j]
+                        
+                        Button(text) {
+                            // emoji selected
+                        }
+                    }
+                }
+            }
+        }
+        .font(.system(size: 64))
+        .padding()
+        .background(.white)
+        .clipShape(.rect(cornerRadius: 20))
+        .fixedSize()
+        .shadow(radius: 10)
     }
 }
 
 #Preview {
-    CardView()
+    CardView(card: ["😁", "😃", "😄", "🥹", "😜", "😎", "😘", "🥸", "🥳"])
 }
