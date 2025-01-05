2397e7460dfcaf80db509e6a6c0bff99237200d1
Oriol Angelet <uriaeri@gmail.com>
2025-01-05 16:38:15 +0100

    Set properties for contentView

--- Twinmoji/Twinmoji/ContentView.swift
+++ Twinmoji/Twinmoji/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //  ContentView.swift
 //  Twinmoji
-//
+//  https://www.youtube.com/live/up6LpJOz5bQ?si=sOrtjm8f7elYvGO_
 //  Created by Uri on 5/1/25.
 //
 
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let allEmoji = Array("emoji").map(String.init)
+    // creates an array of characters
+    let allEmoji = Array("😎🥹🥰😔😂😳🧐🙂😇😅😆😙😬🙃😍🥸😣😶🙄🤨😩😉🥲😋😛🤓😏😭😯😵😐😘😢😠").map(String.init)
+    
+    @State private var currentEmoji: [String] = []
+    
+    @State private var leftCard: [String] = []
+    @State private var rightCard: [String] = []
+    
+    var itemCount: Int
     
     var body: some View {
         VStack {
@@ -23,5 +31,7 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(itemCount: 9)
 }
+
+
--- Twinmoji/Twinmoji/TwinmojiApp.swift
+++ Twinmoji/Twinmoji/TwinmojiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TwinmojiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(itemCount: 9)
         }
     }
 }
