430a8ddc09bbad764ec9f7bb2294523d28e6afcb
Oriol Angelet <uriaeri@gmail.com>
2025-02-15 18:48:15 +0100

    SingleScorecardView iteration

--- Twinmoji/Twinmoji/Presentation/Single/Views/SingleScorecardView.swift
+++ Twinmoji/Twinmoji/Presentation/Single/Views/SingleScorecardView.swift
@@ -14,6 +14,12 @@ struct SingleScorecardView: View {
     var body: some View {
         ZStack {
             Color.orange.opacity(0.7).ignoresSafeArea()
+            
+            VStack(spacing: 10) {
+                CloseSheetButton()
+                // scoreboard as in memoroji
+            }
+            .padding()
         }
     }
 }
