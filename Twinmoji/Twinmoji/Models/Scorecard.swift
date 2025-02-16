9c380812d4c6470be15836716ef66c9ab70a86c7
Oriol Angelet <uriaeri@gmail.com>
2025-02-16 18:53:14 +0100

    Extracted scorecard+stub to its own file

--- Twinmoji/Twinmoji/Models/Scorecard.swift
+++ Twinmoji/Twinmoji/Models/Scorecard.swift
@@ -15,22 +15,3 @@ struct Scorecard: Identifiable, Codable {
     var matches: Int
     var score: Int
 }
-
-#if DEBUG
-extension Scorecard {
-    static var stub: [Scorecard] {
-        [
-            Scorecard(player: "Uri", deck: .animals, matches: 10, score: 400),
-            Scorecard(player: "Darko", deck: .faces, matches: 8, score: 460),
-            Scorecard(player: "Makabre", deck: .flags, matches: 9, score: 400),
-            Scorecard(player: "Larry", deck: .food, matches: 10, score: 40),
-            Scorecard(player: "Oscar", deck: .animals, matches: 5, score: 400),
-            Scorecard(player: "Uri", deck: .faces, matches: 10, score: 4000),
-            Scorecard(player: "Darko", deck: .flags, matches: 7, score: 460),
-            Scorecard(player: "Makabre", deck: .food, matches: 8, score: 400),
-            Scorecard(player: "Lofilldigneeeeeeeeee", deck: .animals, matches: 2, score: 40),
-            Scorecard(player: "Oscar", deck: .faces, matches: 3, score: 400)
-        ]
-    }
-}
-#endif
