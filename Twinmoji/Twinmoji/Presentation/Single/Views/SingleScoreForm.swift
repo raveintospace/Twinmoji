541b190a90bbafea60f2726a879c2a9a8db4426c
Oriol Angelet <uriaeri@gmail.com>
2025-02-21 18:07:37 +0100

    Extracted components to private properties & exiting from SingleScoreForm sets isGameActive to false

--- Twinmoji/Twinmoji/Presentation/Single/Views/SingleScoreForm.swift
+++ Twinmoji/Twinmoji/Presentation/Single/Views/SingleScoreForm.swift
@@ -27,43 +27,11 @@ struct SingleScoreForm: View {
                 TwinmojiGradient()
                 
                 Form {
-                    Section(header: Text("Player name")) {
-                        TextField("Player name", text: $playerName)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.words)
-                            .focused($playerNameFocused)
-                            .submitLabel(.done)
-                            .keyboardType(.alphabet)
-                            .onChange(of: playerName) { _, newValue in
-                                playerName = newValue.filter { $0.isLetter || $0.isNumber }
-                                
-                                if playerName.count > textFieldMaxLength { playerName = String(playerName.prefix(textFieldMaxLength))
-                                }
-                            }
-                    }
-                    Section(header: Text("Deck played")) {
-                        Text("\(viewModel.emojisDeck.rawValue)")
-                    }
-                    Section(header: Text("Total matches")) {
-                        Text("\(viewModel.matches)")
-                    }
-                    Section(header:
-                                Text("Final score")
-                        .bold()
-                        .font(.title)
-                    ) {
-                        Text("\(viewModel.playerScore)")
-                            .bold()
-                            .font(.largeTitle)
-                    }
-                    Section {
-                        Button("Save score") {
-                            saveScore()
-                            dismiss()
-                        }
-                        .foregroundStyle(.blue)
-                        .disabled(playerName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
-                    }
+                    playerNameSection
+                    deckPlayedSection
+                    totalMatchesSection
+                    finalScoreSection
+                    saveScoreButtonSection
                 }
                 .padding()
                 .foregroundStyle(.black)
@@ -77,7 +45,7 @@ struct SingleScoreForm: View {
                     Alert(
                         title: Text("Exit screen"),
                         message: Text("You will lose your score if you press Exit"),
-                        primaryButton: .destructive(Text("Exit")) { dismiss() },
+                        primaryButton: .destructive(Text("Exit")) { exitToMainMenu() },
                         secondaryButton: .default(Text("Keep editing"))
                     )
                 }
@@ -86,10 +54,7 @@ struct SingleScoreForm: View {
             .navigationTitle("")
             .toolbar {
                 ToolbarItem(placement: .principal) {
-                    Text("Save your score")
-                        .font(.title2)
-                        .bold()
-                        .foregroundColor(.white)
+                    singleScoreFormTitle
                 }
             }
             .toolbar {
@@ -119,7 +84,69 @@ extension SingleScoreForm {
         .tint(.white)
     }
     
+    private var singleScoreFormTitle: some View {
+        Text("Save your score")
+            .font(.title2)
+            .bold()
+            .foregroundColor(.white)
+    }
+    
+    private var playerNameSection: some View {
+        Section(header: Text("Player name")) {
+            TextField("Player name", text: $playerName)
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.words)
+                .focused($playerNameFocused)
+                .submitLabel(.done)
+                .keyboardType(.alphabet)
+                .onChange(of: playerName) { _, newValue in
+                    playerName = newValue.filter { $0.isLetter || $0.isNumber }
+                    
+                    if playerName.count > textFieldMaxLength { playerName = String(playerName.prefix(textFieldMaxLength))
+                    }
+                }
+        }
+    }
+    
+    private var deckPlayedSection: some View {
+        Section(header: Text("Deck played")) {
+            Text("\(viewModel.emojisDeck.rawValue)")
+        }
+    }
+    
+    private var totalMatchesSection: some View {
+        Section(header: Text("Total matches")) {
+            Text("\(viewModel.matches)")
+        }
+    }
+    
+    private var finalScoreSection: some View {
+        Section(header: Text("Final score")
+            .bold()
+            .font(.title)
+        ) {
+            Text("\(viewModel.playerScore)")
+                .bold()
+                .font(.largeTitle)
+        }
+    }
+    
+    private var saveScoreButtonSection: some View {
+        Section {
+            Button("Save score") {
+                saveScore()
+                exitToMainMenu()
+            }
+            .foregroundStyle(.blue)
+            .disabled(playerName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+        }
+    }
+    
     private func saveScore() {
         viewModel.saveScore(player: playerName, deck: viewModel.emojisDeck.rawValue, matches: viewModel.matches, score: viewModel.playerScore)
+    }
+    
+    private func exitToMainMenu() {
+        viewModel.isGameActive = false
     }
 }
